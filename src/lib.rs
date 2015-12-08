17fcd1924471bde9f02509b7a8075607fa1e971c
Magnus Hoff <maghoff@gmail.com>
2015-12-08 23:59:00 +0100

    Add a tiny bit of overview documentation to the top level module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+/*!
+PlainTalk
+=========
+This is a library for parsing and generating PlainTalk in Rust.
+
+PlainTalk is a message and field framing protocol. Fields are separated by an
+ASCII space, messages are terminated by a newline (CR LF or LF). For a more
+complete description of PlainTalk, see [PlainTalk &ndash; Introduction and
+Definition](http://magnushoff.com/plaintalk/introduction-and-definition.html).
+*/
+
 pub mod pushparser;
 pub mod pullparser;
 
