2a8f4cf1d7e6069ed21467d03a4eb802501e5b3e
Magnus Hoff <maghoff@gmail.com>
2015-12-22 19:59:31 +0100

    Implement std::error::Error trait for Error

--- src/pullparser/error.rs
+++ src/pullparser/error.rs
@@ -1,10 +1,37 @@
-use std::io;
 use std::convert;
+use std::error;
+use std::fmt;
+use std::io;
 
 #[derive(Debug)]
 pub enum Error {
 	Io(io::Error),
 	Unspecified(&'static str),
+}
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Error::Io(ref err) => write!(f, "IO error: {}", err),
+			Error::Unspecified(ref err) => write!(f, "Unspecified error: {}", err),
+		}
+	}
+}
+
+impl error::Error for Error {
+	fn description(&self) -> &str {
+		match *self {
+			Error::Io(ref err) => err.description(),
+			Error::Unspecified(ref err) => err,
+		}
+	}
+
+	fn cause(&self) -> Option<&error::Error> {
+		match *self {
+			Error::Io(ref err) => Some(err),
+			Error::Unspecified(_) => None,
+		}
+	}
 }
 
 impl convert::From<io::Error> for Error {
